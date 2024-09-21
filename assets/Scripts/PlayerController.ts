eb1642f38141d0f2bcf7d7ce4ebfa559dac52fa5
amadeus711 <952937570@qq.com>
2024-09-21 17:21:33 +0800

    feat: player move

--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -7,7 +7,18 @@ export class PlayerController extends Component {
     input.on(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
   }
 
-  onMouseDown(event: EventMouse) {}
+  onMouseDown(event: EventMouse) {
+    if (event.getButton() === 0) {
+      this.jumpByStep(1);
+    } else if (event.getButton() === 2) {
+      this.jumpByStep(2);
+    }
+  }
+
+  jumpByStep(step: number) {
+    const curPos = this.node.position;
+    this.node.setPosition(curPos.x + step * 40, curPos.y, curPos.z);
+  }
 
   protected onDestroy(): void {
     input.off(Input.EventType.MOUSE_DOWN, this.onMouseDown, this);
